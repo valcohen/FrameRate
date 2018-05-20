6ff1a8de69f8e1c988a33766b906a27dfd8fff8d
Val Cohen <val@valcohen.com>
2018-05-19 17:21:04 -0700

    Spawn nucleons

---
+++ Assets/NucleonSpawner.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class NucleonSpawner : MonoBehaviour {
+
+    public float timeBetweenSpawns  = 0.05f;
+    public float spawnDistance      = 15f;
+    public Nucleon[] nucleonPrefabs;
+
+    float timeSinceLastSpawn;
+
+    // Use this for initialization
+	void Start () {
+		
+	}
+
+    // FixedUpdate keeps spawning independent of frame rate
+	void FixedUpdate () {
+        timeSinceLastSpawn += Time.deltaTime;
+        if (timeSinceLastSpawn >= timeBetweenSpawns) {
+            timeSinceLastSpawn -= timeBetweenSpawns;
+            SpawnNucleon ();
+        }
+	}
+
+    void SpawnNucleon() {
+        // pick a random prefab
+        Nucleon prefab = nucleonPrefabs[Random.Range(0, nucleonPrefabs.Length)];
+
+        // instantiate it
+        Nucleon spawn = Instantiate<Nucleon> (prefab);
+
+        // position it
+        spawn.transform.localPosition = Random.onUnitSphere * spawnDistance;
+    }
+}
